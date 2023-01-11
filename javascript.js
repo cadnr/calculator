15d405fe9bced6e4bfa9447d2e300fccdfbd13e5
nighfy <82936194+nighfy@users.noreply.github.com>
2023-01-11 17:05:15 +0100

    Add operate function

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function operate(operator, number1, number2) {
+    if(operator == '+') {
+        return number1 + number2;
+    }
+    if(operator == '-') {
+        return number1 - number2;
+    }
+    if(operator == '*') {
+        return number1 * number2;
+    }
+    if(operator == '/') {
+        return number1 / number2;
+    }
+}
