eaa30896a787ea985571ae6c5ad10add0b04d140
nighfy <82936194+nighfy@users.noreply.github.com>
2023-01-12 10:08:26 +0100

    Add function to populate the display

--- javascript.js
+++ javascript.js
@@ -12,3 +12,18 @@ function operate(operator, number1, number2) {
         return number1 / number2;
     }
 }
+
+function populateDisplay(button){
+    displayDiv.textContent += button.textContent;
+}
+
+displayDiv = document.querySelector('.display');
+displayDiv.textContent = 'test';
+displayDiv.textContent += 'a';
+
+const numberButtons = document.querySelectorAll('.numbers');
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        populateDisplay(button);
+    });
+});
